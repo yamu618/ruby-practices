2d3a56a3e4018c068cb212c264827927cc9c8bfc
yamu <shoshuto0618@gmail.com>
2025-02-18 01:54:20 +0900

    カレンダーを作成

---
+++ 02.calendar/cal.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+require "date"
+
+def calendar(year, month)
+  firstday = Date.new(year, month, 1)
+  lastday = Date.new(year, month, -1)
+  startday = firstday.wday
+
+  puts "#{month}月 #{year}".center(20)
+  puts "日 月 火 水 木 金 土"
+  print "   " * startday
+  (firstday..lastday).each do |x|
+    print x.day.to_s.rjust(2) + " "
+    puts if x.saturday?
+  end
+  puts
+end
+
+require "optparse"
+
+options = {}
+opt = OptionParser.new
+opt.on("-m MONTH", Integer, "月を指定") { |m| options[:month] = m }
+opt.on("-y YEAR", Integer, "年を指定") { |y| options[:year] = y }
+
+opt.parse!(ARGV)
+
+month = options[:month] || Date.today.month
+year = options[:year] || Date.today.year
+
+calendar(year, month)
