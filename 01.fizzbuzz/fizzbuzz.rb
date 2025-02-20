a09137dfe4366939279610d359e3b5b4739c6042
yamu <shoshuto0618@gmail.com>
2025-02-20 19:20:51 +0900

    numbersを1..20に変更

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,6 +1,5 @@
 def fizzbuzz
-  numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
-  11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+  numbers = 1..20
   numbers.each do |number|
     if number % 3 == 0 && number % 5 == 0
       puts "FizzBuzz"
